ae60584cdfeb174767cb628395c02e914c5ebb92
nabilasuraya <62529745+nabilasuraya@users.noreply.github.com>
2020-09-20 09:58:35 +0700

    upload file web porfolio
    upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const txtElement = ['Freelencer','Web Developer', 'UI/UX Designer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words ='';
+
+(function ngetik(){
+    if(count == txtElement.length){
+        count = 0;
+    }
+    currentTxt = txtElement[count];
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-ngetik').textContent = words;
+
+    if(words.length == currentTxt.length){
+        count++;
+        txtIndex = 0;
+    }
+    setTimeout(ngetik, 200);
+})();
